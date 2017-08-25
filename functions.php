56cd9772f1b7329989120ce18adb28b356d7602f
Stephen Betley <ontiuk@hotmail.com>
2017-08-25 11:56:21 +0100

    iPress Child
    iPress Child Theme for iPressRD and iPressRD2

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * iPress - WordPress Theme Framework                       
+ * ==========================================================
+ *
+ * Theme functions file for iPress Child Theme
+ * - Rename iPress to Child Theme Name
+ * 
+ * @package     iPress\Functions
+ * @link        http://ipress.uk
+ * @license     GPL-2.0+
+ */
+
+// End
